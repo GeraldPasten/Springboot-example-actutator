9bba864586e7224d69dc48a00bd477d503a33399
gpasten <gpasten@redhat.com>
2024-11-21 19:27:32 -0300

    metricas de prometheus

--- src/main/java/com/redhat/demo/spring/services/RestHello.java
+++ src/main/java/com/redhat/demo/spring/services/RestHello.java
@@ -1,14 +1,30 @@
 package com.redhat.demo.spring.services;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestHello {
 
+     // Definimos un logger para la clase
+    private static final Logger logger = LoggerFactory.getLogger(RestHello.class);
+
+
     @GetMapping("/hello") 
     public String greetings() {
-        return "Hello";
+        // Agregamos un log informativo al iniciar la operación
+        logger.info("Endpoint /hello fue invocado");
+
+        String response = "GilBertooo";
+
+         // Agregamos un log de depuración con la respuesta
+         logger.debug("Respuesta generada: {}", response);
+
+
+        return response;
+
     }
 
 }
